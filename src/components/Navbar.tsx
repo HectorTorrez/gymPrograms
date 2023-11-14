db27149a8e3aa54a06f8171afde98c48124999b3
Hector <torrezhectorb@gmail.com>
2023-11-13 20:18:44 -0600

    Add react-router-dom package and implement routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom'
 import { Home, Profile } from './Icons'
 
 export function Navbar (): JSX.Element {
   return (
       <nav>
-        <ul className="flex justify-between m-auto w-[80%] py-5">
-          <li className="flex flex-col items-center font-semibold">
+        <ul className="flex justify-between m-auto w-[80%] max-w-6xl py-5">
+          <Link className="flex flex-col items-center font-semibold" to='/'>
             <p><Home/></p>
             HOME
-          </li>
+          </Link>
           <li className="flex flex-col items-center font-semibold">
             <p><Profile/></p>
             PROFILE
